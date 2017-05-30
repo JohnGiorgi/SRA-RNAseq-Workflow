1e8e8a6021d0674662d6a85f24cb83ed94d15447
JohnGiorgi <johnmgiorgi@gmail.com>
2017-05-29 21:37:38 -0400

    Updated README, refactored some of the code.

--- SRA_RNAseq_Workflow/SRA_rnaseq_workflow.py
+++ SRA_RNAseq_Workflow/SRA_rnaseq_workflow.py
@@ -2,77 +2,75 @@
 
 # TODO(John): Correct styling according to Google's Style Guide
 # TODO(John): Make this script importable (add a main method)
+# TODO(John): Figure out how to end script the correct way
 
 from subprocess import call
-import os  # for changing directory, etc
-import time  # for printing current time to console
-from helpers import cd # for safley changing directories
+import os
+import time
+from helpers import cd
 from argument_parser import parse_args
 
 # parse and store arguments
 arguments = parse_args()
-print(arguments.download)
 
-'''
-# Variables that need to be changed on each run (START)
-# first SRA accension number of the study
-first_SRA = 3407206
-# number of samples in the study
-num_samples = 2
-# study directory
-study = 'study_1'
-# fungal strain
-strain = 'yeast'
-# set of booleans to chose which part of workflow is ran
-downloadSRA = False
-trimSRA = True
-mapSRA = False
-# boolean, true if reads are pair-end
-paired_end = True
-# parent directory
-parent_directory = '/Volumes/Storage/RNASeq_data/' + strain + '/'
-# reference genome directory
-reference_genome = '/Volumes/Storage/RNASeq_data/reference_genomes/' + strain + '_index'
-# (END)
+def download_sra():
+	# check if SRA directory exists, if not create it
+	if not os.path.exists(arguments.dir + '/SRA'):
+		print(time.strftime('[%a, %I:%M]'), 'Creating directory /SRA..')
+		os.makedirs(arguments.dir + '/SRA')
+	# move to newly created directory, download SRA files
+	with cd(arguments.dir + '/SRA'):
+		print(time.strftime("[%a, %I:%M]"), "Downloading SRA file: " + SRA\
+		 + " to " + arguments.dir + '/SRA')
+		# call fastq-dump using the SRA accension.
+		if arguments.paired:
+			call(["fastq-dump", "--split-files", SRA, "-gzip", ">",\
+			"../log/fastq_dump_log_{}.txt".\
+			format(time.strftime("[%a:%I:%M]"))])
+		else:
+			call(["fastq-dump", SRA, "-gzip", ">",\
+			"../log/fastq_dump_log_{}.txt".\
+			format(time.strftime("[%a:%I:%M]"))])
+def trim_sra():
+	# check for SRA folder and file existance
+	if not os.path.exists(arguments.dir + '/SRA'):
+		print('''There is no directory /SRA in {dir}. Either create this
+		directory in {dir} or use the --download flag to create the directory
+		and download SRA files to it'''.format(dir = arguments.dir))
+	if not os.path.exists(os.path.exists(arguments.dir + '/SRA/' + SRA)):
+		print("There is no file {file} in {dir}".\
+		format(file = SRA, dir = arguments.dir))ß
+	# check if SRA_trimmed directory exists, if not create it
+	if not os.path.exists(arguments.dir + '/SRA_trimmed'):
+		print(time.strftime('[%a, %I:%M]'), 'Creating directory /SRA_trimmed..')
+		os.makedirs(arguments.dir + '/SRA_trimmed')
 
-# SRA_files = ['1055985', '1055986', '1055987', '1055988', '1055989', '1055990', '1055991', '1055992', '1055993', '1055994']
-'''
-#######################################################################
+	with cd(arguments.dir + '/SRA_trimmed/'):
+		print(time.strftime("[%a, %I:%M]"), "Trimming " + SRA + ".fastq.gz ...")
+		# call tim_galore  using the SRA accension. Pass flag if paired.
+		if arguments.paired:
+			file_1 = arguments.dir + '/SRA/' + SRA + "_1.fastq.gz"
+			file_1 = arguments.dir + '/SRA/' + SRA + "_2.fastq.gz"
+			call(["trim_galore", file_1, file_2, "--paired", ">",\
+			"../log/trim_galore_output_{}.txt".\
+				format(time.strftime("[%a:%I:%M]"))])
+		else:
+			call(["trim_galore", arguments.dir + '/SRA/' + SRA + \
+			".fastq.gz", ">", "../log/trim_galore_output_{}.txt".\
+			format(time.strftime("[%a:%I:%M]"))])
+def map_sra():
+	pass
 
 for SRA in arguments.accession:
 	# Create string variable SRA, points to SRA accension number
 	SRA = "SRR" + str(SRA)
 	# 1) download SRA file
-	if arguments.download:
-		# check if SRA diretory exists, if not create it
-		if not os.path.exists(arguments.dir + '/SRA'):
-			os.makedirs(arguments.dir + '/SRA')
-		# move to newly created directory, download SRA files
-		with cd(arguments.dir + '/SRA'):
-			print(time.strftime("[%a, %I:%M]"), "Downloading SRA file: " + SRA + " to " + arguments.dir + '/SRA')
-			# call fast-q dump using the SRA accension. Pass --split-files if these are pair-end
-			if arguments.paired:
-				call(["fastq-dump", "--split-files", SRA, "-gzip"])
-			else:
-				call(["fastq-dump", SRA, "-gzip"])
-
-"""
+	if arguments.download: download_sra()
 	# 2) trim file
-	if trimSRA:
-		with cd(parent_directory+study+"/trimmed_fastq/"):
-			# print conformation that moved occured sucsesfully
-			print(time.strftime("[%a, %I:%M]"), "Moving to: ", os.getcwd())
-			print(time.strftime("[%a, %I:%M]"), "Trimming " + SRA + ".fastq.gz ...")
-			# call tim_galore  using the SRA accension. Pass flag if paired.
-			if paired_end:
-				file_1 = "../fastq/" + SRA + "_1.fastq.gz"
-				file_2 = "../fastq/" + SRA + "_2.fastq.gz"
-				call(["trim_galore", file_1, file_2, "--paired"])
-			else:
-				call(["trim_galore", "../fastq/" + SRA + ".fastq.gz"])
-			# print DONE message along with current time
-			print(time.strftime("[%a, %I:%M]"), "DONE.", SRA + ".fastq.gz", "trimmed")
+	if arguments.trim: trim_sra()
+	# 3) map sra file to reference genome
 
+	"""
 	# 3) map back to reference genome
 	if mapSRA:
 		with cd("/Users/johngiorgi/Documents/Google Drive/honours/workflows/tmp/"):
--- SRA_RNAseq_Workflow/argument_parser.py
+++ SRA_RNAseq_Workflow/argument_parser.py
@@ -1,8 +1,15 @@
 # /usr/bin/env python
-
 import argparse
 
+# TODO(John): Go over argparse doc, ensure I am using it to its full potential
+
 def parse_args():
+    '''
+    This method creates an ArgumentParser object, creates and defines all
+    arguments to be used by SRA_rnaseq_workflow.py, and returns all captured
+    arguments as an argparse object.
+    '''
+
     ## create parser
     parser = argparse.ArgumentParser(description='''Run a workflow which includes
     downloading multiple SRA files, trimming them, and mapping them to a reference
@@ -13,13 +20,17 @@ def parse_args():
     parser.add_argument('--accession', metavar = '<accession number>', type = int, \
     nargs='+', help = '''Enter a single accesion number or a list of accession
     numbers''')
-    # paired
+    # paired- or single-end reads
     parser.add_argument('--paired', action = 'store_true', help = '''Changes
     workflow for paired end reads. Default value is False''', default = False)
     # download or local
     parser.add_argument('--download', action = 'store_true', help = '''Downloads
     all SRA files passed by the --accession argument. Default action is to look
     for them in a folder called "SRA" in --dir''', default = False)
+    # to trim or not to trim...
+    parser.add_argument('--trim', action = 'store_true', help = '''Calls
+    trim_galore to preform adaptor timming and quality assurance of all SRA
+    files passed by the --accession argument''', default = False)
     # directory
     parser.add_argument('--dir', metavar = '<directory>', type = str, \
     help = '''The directory where SRA files are downloaded or where they are
@@ -28,6 +39,7 @@ def parse_args():
     # reference genome
     parser.add_argument('--genome', metavar = '<reference genome>', type = str,\
     help = '''Directory to the reference genome''')
+
     ## parse arguments
     arguments = parser.parse_args()
 
